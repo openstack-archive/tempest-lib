756e814e9a2190cb990ff96469b2320853b475f2
Daryl Walleck <daryl.walleck@rackspace.com>
2011-12-05 23:02:08 -0600

    Changes the namespace from storm to tempest, as well as adding addition tests and improvements * Changed namespace from storm to tempest * Added absolute limits service and server personality file tests * Optimized run time for image metadata tests * Added additional assertions for create server and rebuild server tests * Removed any SSH verification until further decisions are made
    Change-Id: I5bebd29be382c9404914c7314302670ae09627fc

---
+++ tempest/exceptions.py
@@ -0,0 +1,33 @@
+class TimeoutException(Exception):
+    """Exception on timeout"""
+    def __repr__(self):
+        return "Request timed out"
+
+
+class BuildErrorException(Exception):
+    """Exception on server build"""
+    def __repr__(self):
+        return "Server failed into error status"
+
+
+class BadRequest(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+        
+        
+class AuthenticationFailure(Exception):
+    msg = ("Authentication with user %(user)s and password "
+           "%(password)s failed.")
+    def __init__(self, **kwargs):
+        self.message = self.msg % kwargs
+
+
+class OverLimit(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
