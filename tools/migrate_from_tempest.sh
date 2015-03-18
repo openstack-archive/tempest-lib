ed27340650455c86c5607508c3588f949fb4c8ae
andreaf <andrea.frittoli@gmail.com>
2015-03-18 18:56:34 +0000

    Support specifying the tempest git URL
    The current implementation uses an hard-coded git:// URL to clone
    tempest from. Supporting specifying an alternate URL via command
    line, as git:// does not work from behind some corporate firewalls.
    
    The default URL - if none is specified - will still be the one
    currently hardcoded in the script.
    
    Change-Id: Ia927f597c719ad0c3e8ae4b59f888e6696d8fead

--- tools/migrate_from_tempest.sh
+++ tools/migrate_from_tempest.sh
@@ -16,7 +16,8 @@ function usage {
     echo "Usage: $0 [OPTION] file1 file2"
     echo "Migrate files from tempest"
     echo ""
-    echo "-o, --output_dir      Specify an directory relative to the repo root to move the migrated files into."
+    echo "-o, --output_dir      Specify a directory relative to the repo root to move the migrated files into."
+    echo "-u, --tempest_git_url Specify the repo to clone tempest from for the migration."
 }
 
 set -e
@@ -27,12 +28,13 @@ while [ $# -gt 0 ]; do
     case "$1" in
         -h|--help) usage; exit;;
         -o|--output_dir) output_dir="$2"; shift;;
+        -u|--tempest_git_url) tempest_git_url="$2"; shift;;
         *) files="$files $1";;
     esac
     shift
 done
 
-TEMPEST_GIT_URL=git://git.openstack.org/openstack/tempest
+TEMPEST_GIT_URL=${tempest_git_url:-git://git.openstack.org/openstack/tempest}
 
 tmpdir=$(mktemp -d -t tempest-migrate.XXXX)
 tempest_lib_dir=$(dirname "$0")
