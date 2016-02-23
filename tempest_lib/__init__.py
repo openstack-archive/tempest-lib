7d669295e056dfd9ffb5d33e1ae7ec37512b48a8
Andrea Frittoli (andreaf) <andrea.frittoli@hpe.com>
2016-02-23 11:26:56 -0500

    Deprecation warning in tempest-lib
    Add a depreaction warning to tempest-lib to let our users know
    they should switch to tempest.lib if they want the latest and
    greatest fixes and functionalities.
    
    Partially implements bp tempest-lib-reintegration
    
    Change-Id: I316f5b3c00d50345761b7678297b894730d968e5

--- tempest_lib/__init__.py
+++ tempest_lib/__init__.py
@@ -12,8 +12,19 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import pbr.version
+import warnings
 
+import pbr.version
 
 __version__ = pbr.version.VersionInfo(
     'tempest_lib').version_string()
+
+# Emit a warning for tempest-lib deprecation. We want the warning to
+# be displayed only once.
+warnings.simplefilter('once', category=DeprecationWarning)
+warnings.warn(
+    'tempest-lib is deprecated for future bug-fixes and code changes in favor '
+    'of tempest. Please change your imports from tempest_lib to tempest.lib',
+    DeprecationWarning)
+# And back to normal!
+warnings.resetwarnings()
